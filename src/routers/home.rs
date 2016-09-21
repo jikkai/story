4e2cfbf854a2c218d2e9a0d8d072f4c75cb78314
十界 <sonne@asaki.me>
2016-09-22 00:01:03 +0800

    默认主题：添加分页 & 文章头体解析

--- src/routers/home.rs
+++ src/routers/home.rs
@@ -19,7 +19,11 @@ use self::hbs::Template;
 struct Posts {
 	list: Json,
 	count: usize,
-	page: Vec<i32>
+	page: Vec<i32>,
+	prev: usize,
+	next: usize,
+	is_not_start: bool,
+	is_not_end: bool
 }
 
 impl ToJson for Posts {
@@ -28,6 +32,10 @@ impl ToJson for Posts {
 		m.insert("list".to_string(), self.list.to_json());
 		m.insert("count".to_string(), self.count.to_json());
 		m.insert("page".to_string(), self.page.to_json());
+		m.insert("prev".to_string(), self.prev.to_json());
+		m.insert("next".to_string(), self.next.to_json());
+		m.insert("is_not_start".to_string(), self.is_not_start.to_json());
+		m.insert("is_not_end".to_string(), self.is_not_end.to_json());
 		Json::Object(m)
 	}
 }
@@ -65,15 +73,15 @@ pub fn home(req: &mut Request) -> IronResult<Response> {
 		let file_name = path.file_name().unwrap().to_str().unwrap();
 
 		if path.is_dir() == false && file_name.contains(".md") {
-			let regex = Regex::new(r"title[\s\S]*--*\n").unwrap();
 
 			let mut file = File::open(("./src/posts/".to_string() + &file_name).as_str()).unwrap();
 			let mut buffer = String::new();
 			file.read_to_string(&mut buffer).unwrap();
-
+			
+			let regex = Regex::new(r"title[\s\S]*--*\n").unwrap();
 			let header = regex.captures(buffer.as_str()).unwrap().at(0).unwrap();
 
-			let regex_date = Regex::new(r"date:\s\S*\s?\S*\n").unwrap();
+			let regex_date = Regex::new(r"date:\s\S*\s").unwrap();
 			let regex_tags = Regex::new(r"tags:\s\[(\S*,?\s?)*\]\n").unwrap();
 			let date: Vec<&str> = regex_date.captures(header).unwrap().at(0).unwrap().split("date: ").collect();
 			let tags: Vec<&str> = regex_tags.captures(header).unwrap().at(0).unwrap().split("tags: ").collect();
@@ -91,7 +99,13 @@ pub fn home(req: &mut Request) -> IronResult<Response> {
 	mv.sort_by(|a, b| b.date.cmp(&a.date.to_string()));
 
 	let size = index.to_string().parse::<usize>().unwrap() - 1;
-	let page = mv.len().to_string().parse::<i32>().unwrap() / 10;
+	let mut page = mv.len().to_string().parse::<i32>().unwrap();
+
+	if page % 10 == 0 {
+		page = page / 10;
+	} else {
+		page = page / 10 + 1;
+	}
 
 	let mut page_v = vec![];
 	for i in 1..(page + 1) {
@@ -99,12 +113,20 @@ pub fn home(req: &mut Request) -> IronResult<Response> {
 	}
 
 	let v = mv.split_at(size * 10).1;
-	let list = v.split_at(10).0;
+	let mut length = 10;
+	if v.len() < 10 {
+		length = v.len()
+	}
+	let list = v.split_at(length).0;
 
 	let posts = Posts {
 		list: list.to_json(),
 		count: mv.len(),
-		page: page_v
+		page: page_v,
+		prev: size,
+		next: size + 2,
+		is_not_start: size != 0,
+		is_not_end: size.to_string().parse::<i32>().unwrap() != page - 1
 	};
 
 	let mut resp = Response::new();
--- src/routers/post.rs
+++ src/routers/post.rs
@@ -1,5 +1,6 @@
 extern crate iron;
 extern crate router;
+extern crate regex;
 extern crate url;
 extern crate hoedown;
 extern crate rustc_serialize as rss;
@@ -12,6 +13,7 @@ use std::fs::File;
 use self::iron::prelude::*;
 use self::iron::status;
 use self::router::Router;
+use self::regex::Regex;
 use self::url::percent_encoding::percent_decode;
 use self::hoedown::{ Markdown, Render };
 use self::hoedown::renderer::html::{ self, Html };
@@ -20,6 +22,9 @@ use self::hbs::Template;
 
 struct Post {
 	id: String,
+	title: String,
+	date: String,
+	tags: String,
 	content: String
 }
 
@@ -27,6 +32,9 @@ impl ToJson for Post {
 	fn to_json(&self) -> Json {
 		let mut m = BTreeMap::new();
 		m.insert("id".to_string(), self.id.to_json());
+		m.insert("title".to_string(), self.title.to_json());
+		m.insert("date".to_string(), self.date.to_json());
+		m.insert("tags".to_string(), self.tags.to_json());
 		m.insert("content".to_string(), self.content.to_json());
 		Json::Object(m)
 	}
@@ -41,15 +49,31 @@ pub fn post(req: &mut Request) -> IronResult<Response> {
 	let mut buffer = String::new();
 	file.read_to_string(&mut buffer).unwrap();
 
+	// 解析文件头
+	let regex = Regex::new(r"title[\s\S]*--*\n").unwrap();
+	let header = regex.captures(buffer.as_str()).unwrap().at(0).unwrap();
+
+	let regex_date = Regex::new(r"date:\s\S*\s").unwrap();
+	let regex_tags = Regex::new(r"tags:\s\[(\S*,?\s?)*\]\n").unwrap();
+	let date: Vec<&str> = regex_date.captures(header).unwrap().at(0).unwrap().split("date: ").collect();
+	let tags: Vec<&str> = regex_tags.captures(header).unwrap().at(0).unwrap().split("tags: ").collect();
+
+	// 解析文件体
+	let regex_content = Regex::new(r"---\n[\s\S]*").unwrap();
+	let content = regex_content.captures(buffer.as_str()).unwrap().at(0).unwrap();
+
 	// 解析markdown文件
-	let doc = Markdown::new(buffer.to_string().as_str());
+	let doc = Markdown::new(content.to_string().as_str());
   let mut html = Html::new(html::Flags::empty(), 0);
 	let res = html.render(&doc);
-	let content = res.to_str().unwrap();
+	let result = res.to_str().unwrap();
 
 	let post = Post {
 		id: id.to_string(),
-		content: content.to_string()
+		title: title.to_string(),
+		date: date[1].to_string(),
+		tags: tags[1].to_string(),
+		content: result.to_string()
 	};
 
 	let mut resp = Response::new();
